a10bb6454772c076abaabd5dbf9634b580de3f4f
Yuki Furuyama <addsict@gmail.com>
2018-07-04 23:42:49 +0900

    reafactor: naming

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const ORIGINAL_DOC_LANG = 'en';
 
-function constructOriginalDocUrl(url) {
+function makeOriginalUrl(url) {
   const base = url.split('?')[0];
   return base + '?hl=' + ORIGINAL_DOC_LANG;
 }
 
 function fetchOriginalDoc(url) {
-  const originalUrl = constructOriginalDocUrl(url);
+  const originalUrl = makeOriginalUrl(url);
   return fetch(originalUrl)
     .then(res => res.text())
     .then(text => new DOMParser().parseFromString(text, "text/html"));
@@ -21,37 +21,44 @@ function extractLastUpdated(doc) {
   }
 }
 
-function alertOutdated(currentLastUpdated, originalLastUpdated, originalDocUrl) {
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  const currentDate = `${monthNames[currentLastUpdated.getMonth()]} ${currentLastUpdated.getDate()}, ${currentLastUpdated.getFullYear()}`;
-  const originalDate = `${monthNames[originalLastUpdated.getMonth()]} ${originalLastUpdated.getDate()}, ${originalLastUpdated.getFullYear()}`;
-  const template = `
+function alertOutdated(currentUpdated, originalUpdated, originalUrl) {
+  const months = [
+    "January", "February", "March",
+    "April", "May", "June",
+    "July", "August", "September",
+    "October", "November", "December",
+  ];
+
+  const currentDate = `${months[currentUpdated.getMonth()]} ${currentUpdated.getDate()}, ${currentUpdated.getFullYear()}`;
+  const originalDate = `${months[originalUpdated.getMonth()]} ${originalUpdated.getDate()}, ${originalUpdated.getFullYear()}`;
+  const alert = `
     <div style="width: 210px; height: 85px; background-color: red; color: white; padding: 10px; font-size: 14px; position: fixed; bottom: 5px; right: 0px; z-index: 99999999;">
       <span style="font-weight: bold;">Warning</span>: outdated document
       <div style="padding: 5px 0px 0px 0px; line-height: 1.3">
         This: ${currentDate}.<br/>
-        <a href="${originalDocUrl}" target="_blank" style="color: white; text-decoration: underline;">Original</a>: ${originalDate}.
+        <a href="${originalUrl}" target="_blank" style="color: white; text-decoration: underline;">Original</a>: ${originalDate}.
       </div>
     </div>
   `;
-  var div = document.createElement( "div" );
-  div.innerHTML = template;
+
+  const div = document.createElement('div');
+  div.innerHTML = alert;
   document.body.appendChild(div);
 }
 
 (function() {
   const url = window.location.href;
   fetchOriginalDoc(url).then(doc => {
-    const currentLastUpdated = extractLastUpdated(document);
-    const originalLastUpdated = extractLastUpdated(doc);
+    const currentUpdated = extractLastUpdated(document);
+    const originalUpdated = extractLastUpdated(doc);
 
-    if (!(currentLastUpdated && originalLastUpdated)) {
+    if (!(currentUpdated && originalUpdated)) {
       console.error('failed to get last updated date');
       return;
     }
 
-    if (currentLastUpdated.getTime() < originalLastUpdated.getTime()) {
-      alertOutdated(currentLastUpdated, originalLastUpdated, constructOriginalDocUrl(url));
+    if (currentUpdated.getTime() < originalUpdated.getTime()) {
+      alertOutdated(currentUpdated, originalUpdated, makeOriginalUrl(url));
     }
   }).catch(e => {
     console.error('failed to fetch original gcp document: ' + e);
